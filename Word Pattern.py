897740834c3c53936288ec99596ca52b1258760f
mahaoyang <mahydxygitlab>
2018-09-12 16:05:47 +0800

    init

--- Word Pattern.py
+++ Word Pattern.py
@@ -6,16 +6,16 @@ class Solution:
         :rtype: bool
         """
         t = str.split(' ')
-        a = map(pattern.find, pattern)
-        b = map(t.index, t)
-        a = list(a)
-        b = list(b)
+        # a = map(pattern.find, pattern)
+        # b = map(t.index, t)
+        # a = list(a)
+        # b = list(b)
         return list(map(pattern.find, pattern)) == list(map(t.index, t))
 
 
-a = Solution().wordPattern(pattern="abba", str="dog cat cat dog")
-print(a)
-
-a = 'aabb'
-b = a.find(a)
-print(b)
+# a = Solution().wordPattern(pattern="abba", str="dog cat cat dog")
+# print(a)
+#
+# a = 'aabb'
+# b = a.find(a)
+# print(b)
