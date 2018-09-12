f756827f78a524798e78755ad11da18ccb9d11de
mahaoyang <mahydxygitlab>
2018-09-12 18:49:15 +0800

    init

---
+++ 左移位字符串.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def LeftRotateString(self, s, n):
+        # write code here
+        length = len(s)
+        if n <= 0 or length == 0:
+            return s
+        if n > length:
+            n = n % length
+        return s[n:] + s[:n]
